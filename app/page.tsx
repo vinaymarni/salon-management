b8a6a2f14459d94779d4d9cc1ef649f931271042
rpc-vinay <131428813+rpc-vinay@users.noreply.github.com>
2026-05-13 22:47:55 +0530

    landing page added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { redirect } from 'next/navigation'
+// import { redirect } from 'next/navigation'
 import FirstPage from './FirstPage'
 
 export default function Home() {
-  // return <FirstPage />
-  redirect('/dashboard')
+  return <FirstPage />
+  // redirect('/dashboard')
 }
